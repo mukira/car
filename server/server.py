1285cbaa32442ba633d5416f9521d6c9e6bbc034
bjorke <bjrkie@gmail.com>
2014-03-13 12:11:18 +0100

    trying diffrent sockets

--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ __author__ = 'ludwig'
 
 import socket               # Import socket module
 
-s = socket.socket(socket.AF_INET6)         # Create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
