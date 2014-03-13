857cb3e31820da1782ed9e85d8feaa4e089440cc
bjorke <bjrkie@gmail.com>
2014-03-13 12:30:54 +0100

    Prints if there's any data in the object

--- server/server.py
+++ server/server.py
@@ -7,12 +7,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
 conn, addr = s.accept()
-print 'Connected by', addr
+print "Connected by", addr
 
 while True:
     data = conn.recv(1024)
-    repr(data)
+    if (repr(data) != None):
+        print "recived data;" , repr(data)
     conn.sendall(data)
-
-
 conn.close()
