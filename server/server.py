685052570cf4091484be7a9b049b953af0f01f40
bjorke <bjrkie@gmail.com>
2014-03-13 12:25:18 +0100

    listen has to be 1

--- server/server.py
+++ server/server.py
@@ -5,7 +5,7 @@ HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
-s.listen()
+s.listen(1)
 conn, addr = s.accept()
 print 'Connected by', addr
 
