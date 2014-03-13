cebce620dc9b74fd41d7a819d1996edf03c11464
bjorke <bjrkie@gmail.com>
2014-03-13 12:16:15 +0100

    rewritten code

--- client/client.py
+++ client/client.py
@@ -1,17 +1,11 @@
-__author__ = 'ludwig'
-#!/usr/bin/python           # This is client.py file
+# Echo client program
+import socket
 
-import socket               # Import socket module
-
-s = socket.socket()         # Create a socket object
-#host = socket.gethostname() # Get local machine name
-host = "192.168.1.66:12345"
-#port = 12345                # Reserve a port for your service.
-
-s.connect((host))
-print "Response from server; " + s.recv(1024)
-s.send("here's some data!")
-
-
-s.close                     # Close the socket when done
-
+HOST = 'raspberrypi.lan'    # The remote host
+PORT = 50007              # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall('Hello, world')
+data = s.recv(1024)
+s.close()
+print 'Received', repr(data)
--- server/server.py
+++ server/server.py
@@ -1,17 +1,15 @@
-__author__ = 'ludwig'
-#!/usr/bin/python           # This is server.py file
+# Echo server program
+import socket
 
-import socket               # Import socket module
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
-
-s.listen(5)                 # Now wait for client connection.
-while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print 'Got connection from', addr
-   c.send('Thank you for connecting')
-   print(addr)
-   #c.close()                # Close the connection
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 50007              # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print 'Connected by', addr
+while 1:
+    data = conn.recv(1024)
+    if not data: break
+    conn.sendall(data)
+conn.close()
