9e5107088856bfa249d52873dffed35591bfc21f
bjorke <bjrkie@gmail.com>
2014-03-13 12:09:37 +0100

    test updates

--- client/client.py
+++ client/client.py
@@ -5,10 +5,10 @@ import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
 #host = socket.gethostname() # Get local machine name
-host = '192.168.1.66'
-port = 12345                # Reserve a port for your service.
+host = "192.168.1.66:12345"
+#port = 12345                # Reserve a port for your service.
 
-s.connect((host, port))
+s.connect((host))
 print "Response from server; " + s.recv(1024)
 s.send("here's some data!")
 
