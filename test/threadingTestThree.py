e1f51b93131b9df798b93be2eabdc347bd5e1e07
bjorke <bjrkie@gmail.com>
2014-03-21 21:34:30 +0100

    updates to make several threads run at the same time

--- test/threadingTestThree.py
+++ test/threadingTestThree.py
@@ -4,9 +4,9 @@ import threading
 import time
 from threading import Thread, Event
 #global variables
-threadLock = threading.Lock()
+#threadLock = threading.Lock()
 threadsArray = []
-
+loopVariable = 1
 class myThread (threading.Thread):
     def __init__(self, threadID, name):
         threading.Thread.__init__(self)
@@ -15,25 +15,30 @@ class myThread (threading.Thread):
     def run(self):
         print ("Starting thread; " + self.name)
         # Get lock to synchronize threads
-        threadLock.acquire()
+        #threadLock.acquire() #prevents any new threads from spawning
         if self.name == "forward":
             forward()
         elif self.name == "back":
             back()
         #print_time(self.name, self.counter, 3)
-        # Free lock to release next thread
-        threadLock.release()
+        #threadLock.release() # Free lock to release next thread
+
 
 def forward():
+    global loopVariable
     while True:
-        print("Running method forward!")
-        time.sleep(5)
-        break
+        print"Running method forward! ",loopVariable, "\n"
+        time.sleep(1)
+        if loopVariable > 20:
+            break
 def back():
+    global loopVariable
     while True:
-        print("Running method back!")
-        time.sleep(5)
-        break
+        print"Running method back! ",loopVariable, "\n"
+        time.sleep(1)
+        loopVariable += 1
+        if loopVariable > 20:
+            break
         
 def parent():
 
@@ -44,7 +49,13 @@ def parent():
 
     #print("thread1",thread1.isAlive())
     #print("thread2",thread2.isAlive())
+    thread1.start()
+    thread2.start()
+    threadsArray.append(thread1)
+    threadsArray.append(thread2)
+
     
+    '''
     if thread1.isAlive() == False and thread2.isAlive() == False:
         # Start new Thread
         thread1.start()
@@ -55,9 +66,11 @@ def parent():
         thread2.start()
         # Add thread to thread list
         threadsArray.append(thread2)
-
+    '''
     # Wait for all threads to complete
     while True:
+        #print("loop in the parrent thread",threadsArray)
+        #time.sleep(1)
         for t in threadsArray:
             t.join()
         print "Exiting Main Thread"
@@ -67,3 +80,4 @@ if __name__ == "__main__":
     par = Thread(target=parent, args=())
     print("starting parent thread")
     par.start()
+    print("parent thread started")
