735b8e2306779ed93ac20f6a27c50c38aa6d4d4d
bjorke <bjrkie@gmail.com>
2014-03-21 20:15:54 +0100

    updates

--- test/threadingTestThree.py
+++ test/threadingTestThree.py
@@ -1,48 +1,65 @@
 #!/usr/bin/python
-import Queue
+
 import threading
 import time
-
+from threading import Thread, Event
 #global variables
-workQueue = Queue.Queue(10)
-exitFlag = 0
-queueLock = threading.Lock()
+threadLock = threading.Lock()
+threads = []
 
-class sThread (threading.Thread):
-    def __init__(self, threadID, name, q):
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, counter):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
-        self.q = q
+        self.counter = counter
     def run(self):
-        print ("Starting " + self.name)
+        print "Starting " + self.name
+        # Get lock to synchronize threads
+        threadLock.acquire()
         if self.name == "forward":
             forward()
         elif self.name == "back":
             back()
-        print "Exiting " + self.name
-        
-def process_data(threadName, q):
-    while not exitFlag:
-        queueLock.acquire()
-        if not workQueue.empty():
-            data = q.get()
-            queueLock.release()
-            print "%s processing %s" % (threadName, data)
-        else:
-            queueLock.release()
-        time.sleep(1)
-        
+        #print_time(self.name, self.counter, 3)
+        # Free lock to release next thread
+        threadLock.release()
 
 def forward():
-    print("Method forward!")
+    while True:
+        print("Running method forward!")
+        time.sleep(5)
+        break
 def back():
-    print("Method back!")
+    while True:
+        print("Running method back!")
+        time.sleep(5)
+        break
+        
+def parent():
+
+
+    # Create new threads
+    thread1 = myThread(1, "forward", 1)
+    thread2 = myThread(2, "back", 2)
+
+    if not thread1.isAlive() or thread2.isAlive():
+        # Start new Threads
+        thread1.start()
+        # Add threads to thread list
+        threads.append(thread1)
+    if not thread1.isAlive() or thread2.isAlive():
+        thread2.start()
+        threads.append(thread2)
+
+    # Wait for all threads to complete
+    while True:
+        for t in threads:
+            t.join()
+        print "Exiting Main Thread"
+        break
 
 if __name__ == "__main__":
-    threadID = 1
-    tName = "forward"
-    thread = sThread(threadID, tName, workQueue)
-    thread.start()
-    if thread.isAlive():
-        print("threadName; " + thread.name)
+    par = Thread(target=parent, args=())
+    print("starting parent thread")
+    par.start()
