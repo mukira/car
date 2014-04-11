c8a69ee830dcf997d1fdb6db3a6bbab1f7a63a6f
bjorke <bjrkie@gmail.com>
2014-04-11 10:16:41 +0200

    added threading

--- server/CarController.py
+++ server/CarController.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-from datetime import datetime
+from datetime import datetime,gmtime, strftime
 import threading
 from threading import Thread, Event
 
@@ -20,6 +20,8 @@ GPIO.setmode(GPIO.BCM)
 # Select if pins are input or output
 GPIO.setup(rightWheelForward, GPIO.OUT)
 GPIO.setup(leftWheelForward,GPIO.OUT)
+#keep track of all the threads
+threadsArray = []
 
 def forward():
     print("in method forward")
@@ -63,7 +65,34 @@ def runCarForward():
       print datetime.now().strftime('%Y-%m-%d %H:%M:%S')
    #when the loop stops turn off the pins
       turnOffPins()
+      
+class startChildThread (threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+       if self.name == "forward":
+            runCarForward()
+       #elif self.name == "back":
+       #     back()
 
+
+       
+def mainThread():
+    #Define threads
+    thread1 = startChildThread(1, "forward")
+    thread2 = startChildThread(2, "back")
+
+    # Wait for all threads to complete
+    while True:
+        #print("loop in the parrent thread",threadsArray)
+        #time.sleep(1)
+        for t in threadsArray:
+            t.join()
+        print "Exiting Main Thread"
+        break
+    
 
 def runCar(delayTime,direction):
     delayTimeGlo = delayTime
