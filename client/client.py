86c3c8bf6d86041b31ce8b3a1dd2a81caea32167
bjorke <bjrkie@gmail.com>
2014-05-12 23:34:21 +0200

    updates

--- client/client.py
+++ client/client.py
@@ -3,13 +3,13 @@ import socket
 import time
 from datetime import datetime
 
-HOST = "raspberrypi.lan"    # The remote host
+HOST = "10.0.1.8"    # The remote host
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect((HOST, PORT))
 
 print datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "sending"
-s.sendall("exit")
+s.sendall("right")
 print "Received data; ", s.recv(1024)
 s.close()
